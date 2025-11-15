9ab96779acc36b5f244162dcf99fad27e997b9ca
EstanislaoE <edgar1estanislao@gmail.com>
2025-11-15 08:12:42 -0800

    working jjst fine

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -2,9 +2,19 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+import { parseFor } from "postcss-syntax";
+
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    files: ["**/*.css"],
+    languageOptions: {
+      parser: parseFor("css"),
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
