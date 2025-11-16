2ce315f2f8f20879704c2842daa49b0722e43976
EstanislaoE <edgar1estanislao@gmail.com>
2025-11-15 20:38:58 -0800

    added pic

--- client/src/app/(nondashboard)/landing/HeroSection.tsx
+++ client/src/app/(nondashboard)/landing/HeroSection.tsx
@@ -5,12 +5,25 @@ const HeroSection = () => {
   return (
     <div className="relative h-screen">
       <Image
-        src="/landing-splash.jpg"
+        src="/patrick-hendry-mevy.jpg"
         alt="Rentiful Rental Platform Hero Section"
         fill
         className="object-cover object-center"
         priority
       />
+
+      {/* --- Photo Credits Overlay --- */}
+      <div className="absolute bottom-4 right-4 z-10 text-white text-xs bg-black p-1 rounded">
+        Photo by{" "}
+        <a
+          href="https://unsplash.com/@worldsbetweenlines?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline hover:text-white"
+        >
+          Patrick Hendry
+        </a>
+      </div>
     </div>
   );
 };
