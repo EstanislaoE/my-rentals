9463486569b9d78ab45a36d549db6ab94d093aec
EstanislaoE <edgar1estanislao@gmail.com>
2025-11-15 18:23:50 -0800

    mick insereted

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,41 @@
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div
+      className="fixed top-0 left-0 w-full z-50 shadow-xl "
+      style={{ height: `${NAVBAR_HEIGHT}px` }}
+    >
+      <div className="flex justify-between items-center w-full py-3 px-8 bg-primary-700 text-white">
+        <div className="flex items-center gap-4 md:gap-6">
+          <Link
+            href="/"
+            className="cursor-pointer hover:!text-primary-300"
+            scroll={false}
+          >
+            <div className="flex items-center gap-3">
+              <Image
+                src="/mick.svg"
+                alt="Rentiful Logo"
+                width={52}
+                height={66}
+                className="w-6 h-6"
+              />
+              <div className="text-xl font-bold">
+                Rent
+                <span className="text-secondary-500 font-light hover:!text-primary-300">
+                  IFUL
+                </span>
+              </div>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
